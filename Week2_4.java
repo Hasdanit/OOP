9c0a4042a0bd2aec77075c7b1e63dd6d0780194a
Hasdanit <230547@astanait.edu.kz>
2023-12-18 17:58:52 +0600

    F

--- Week2_4.java
+++ Week2_4.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Week2_4 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        System.out.println(x*x + y - z/2);
     }
 }
